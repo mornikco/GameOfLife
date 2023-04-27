56e3cafdeb28aa740ef1e134b3986da5a103d7b3
Daniele Megna <megna.dany@gmail.com>
2023-04-27 19:11:38 +0200

    Implement WorldDeserializer.from(worldString)

--- src/main/kotlin/World.kt
+++ src/main/kotlin/World.kt
@@ -1,3 +1,4 @@
+
 data class World(private val aliveCellCoordinates: List<Coordinate> = emptyList()) {
 
     override fun toString(): String {
@@ -5,7 +6,7 @@ data class World(private val aliveCellCoordinates: List<Coordinate> = emptyList(
         for (y in 0 until WORLD_VIEW_DIMENSION) {
             for (x in 0 until WORLD_VIEW_DIMENSION) {
                 val current = Coordinate(x, y)
-                result += if (aliveCellCoordinates.contains(current)) "X" else "."
+                result += if (aliveCellCoordinates.contains(current)) ALIVE_CELL_CHAR else DEAD_CELL_CHAR
             }
             result += "\n"
         }
@@ -14,6 +15,9 @@ data class World(private val aliveCellCoordinates: List<Coordinate> = emptyList(
     }
 
     companion object {
+        const val ALIVE_CELL_CHAR = 'X'
+        const val DEAD_CELL_CHAR = '.'
+
         private const val WORLD_VIEW_DIMENSION = 3
     }
 }
--- src/main/kotlin/WorldDeserializer.kt
+++ src/main/kotlin/WorldDeserializer.kt
@@ -1,6 +1,18 @@
 class WorldDeserializer {
     fun from(worldString: String): World {
-        return World(emptyList())
+        val aliveCoordinates = worldString
+            .split("\n")
+            .mapIndexed { y, row ->
+                row.mapIndexed { x, cellChar ->
+                    if (cellChar == World.ALIVE_CELL_CHAR)
+                        Coordinate(x, y)
+                    else
+                        null
+                }
+            }
+            .flatten()
+            .filterNotNull()
+        return World(aliveCoordinates)
     }
 
 }
